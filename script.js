81e4fdf7464ea08de0e22b47528e6ade7d616b15
P. Colin Manikoth <pmanikoth@ewu.edu>
2020-10-12 21:16:38 -0700

    Template

---
+++ script.js
@@ -0,0 +1,30 @@
+const playSound = e => {
+      let keyCode;
+      if (e.type === 'keydown') {
+        keyCode = e.keyCode;
+      } else {
+        keyCode = e.target.getAttribute('data-key') || e.target.parentNode.getAttribute('data-key');
+      }
+      const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+      const key = document.querySelector(`div[data-key="${keyCode}"]`);
+
+      if (!audio) return;
+
+      key.classList.add('playing');
+      audio.currentTime = 0;
+      audio.play();
+    }
+
+    const removeTransition = e => {
+      if (e.propertyName !== 'transform') return;
+      e.target.classList.remove('playing');
+    }
+
+    const keys = Array.from(document.querySelectorAll('.key'));
+    keys.forEach(key => {
+      key.addEventListener('transitionend', removeTransition)
+    });
+
+    window.addEventListener('keydown', playSound);
+    window.addEventListener('touchstart', playSound);
+    window.addEventListener('click', playSound);
